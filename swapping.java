3564c7e60e56e14250231ba0afcf6b80a84bbdbb
revathithirumal <revathit2003@gmail.com>
2025-03-13 14:59:31 +0530

    fix swapping temp

---
+++ swapping.java
@@ -0,0 +1,16 @@
+public class swapping{
+    public static void main(String[] args){
+        int x=7;
+        int y=10; int temp;
+        System.out.println("Before swap");
+        System.out.println("x=:"+x);
+        System.out.println("y=:"+y);
+        temp=x;
+        x=y;
+        temp=y;
+        System.out.println("After swap");
+        System.out.println("x=:"+x);
+        System.out.println("y=:"+y);
+    }
+
+}
