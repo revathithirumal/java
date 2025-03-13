57dc48b9a8b7f5ccca507da1bf9410f56e6c8576
revathithirumal <revathit2003@gmail.com>
2025-03-13 10:32:42 +0530

    fix message

---
+++ baby.java
@@ -0,0 +1,7 @@
+public class baby{
+    public static void main(String[] args){
+    int myint=99;
+    double mydouble=myint;
+    System.out.println(mydouble);
+}
+}
---
+++ details.java
@@ -0,0 +1,10 @@
+public class details{
+    public static void main(String[]args){
+        String name="revathi";
+        int mark=89;
+        char grade='A';
+        System.out.println("My name is:"+name);
+        System.out.println(mark);
+        System.out.println(grade);
+    }
+}
