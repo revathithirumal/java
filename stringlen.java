50c3da2ba887da086154b83b474ca60f850d5496
revathithirumal <revathit2003@gmail.com>
2025-03-13 14:36:52 +0530

    fix apple

---
+++ stringlen.java
@@ -0,0 +1,7 @@
+public class stringlen {
+    public static void main(String[] args) {
+        String fruit = "apple";
+        int length = fruit.length();
+        System.out.println("Length of the string:"+length);
+    }
+}
