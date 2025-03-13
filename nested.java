cf58f6c383d0db932236ceafa44878695837edda
revathithirumal <revathit2003@gmail.com>
2025-03-13 15:40:45 +0530

    fix i am

---
+++ nested.java
@@ -0,0 +1,8 @@
+public class nested {
+    public static void main(String[] args) {
+        char v='a'; 
+        if(v=='a'||v=='e'||v=='i'||v=='o'||v=='u') {
+            System.out.println("Vowel");
+        }
+    }
+}
