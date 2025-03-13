7027bf72440d4572336fa1916e1cd0d09b8bec44
revathithirumal <revathit2003@gmail.com>
2025-03-13 15:13:36 +0530

    fit age

---
+++ condition.java
@@ -0,0 +1,14 @@
+public class condition{
+    public static void main(String[] args){
+        int age=21;
+        System.out.println("age="+age);
+        if(age>21)
+        {
+            System.out.println("You are elgible to vote");
+        }
+        else
+        {
+            System.out.println("You are eligible to vote");
+        }
+    }
+}
