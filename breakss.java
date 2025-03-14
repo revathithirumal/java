6f293cba3e9cdb01854b94d231a22da70c5e23d6
revathithirumal <revathit2003@gmail.com>
2025-03-14 11:49:17 +0530

    fix breaksss

---
+++ breakss.java
@@ -0,0 +1,14 @@
+public class breakss
+{
+    public static void main(String[] args)
+    {
+        for(int i=0;i<=10;i++)
+        {
+        if(i==7)
+        {
+            break;
+        }
+        System.out.println(i); 
+    }
+}
+}
