27489dc1c21555450d7c03d88f197290ca16e29c
revathithirumal <revathit2003@gmail.com>
2025-03-14 13:30:54 +0530

    fix mom

---
+++ mult.java
@@ -0,0 +1,11 @@
+public class mult{
+    static void multiply()
+    {
+        int x=20,y=10;
+        int z=x*y;
+        System.out.println("the result"+z);
+    }
+    public static void main(String[] args){
+        multiply();
+    }
+}
