6c322feff3f95488cff4c9486fa5f530f4c78511
revathithirumal <revathit2003@gmail.com>
2025-03-14 11:02:52 +0530

    fix three orenge

---
+++ tr.java
@@ -0,0 +1,13 @@
+public class tr
+{
+    public static void main(String[] args)
+    {
+        int i=3;
+        String fruit="orenge";
+        do{
+            System.out.println(fruit);
+            i++;
+        }
+        while(i<=5);
+    }
+}
