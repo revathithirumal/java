296988eb5d71e87ff10e4f22339c4a8c8991d6f4
revathithirumal <revathit2003@gmail.com>
2025-03-14 14:29:55 +0530

    fix threeary

---
+++ threear.java
@@ -0,0 +1,8 @@
+public class threear
+{
+    public static void main(String[] args)
+    {
+        int[][]numbers={{1,2,3,4,5},{6,7,8,9,0},{5,4,7,5,3}};
+        System.out.println(numbers[2][3]);
+    }
+}
