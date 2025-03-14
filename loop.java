d1b2bea59d3df54fd846d680c28f60965ae84db8
revathithirumal <revathit2003@gmail.com>
2025-03-14 10:32:06 +0530

    fix loopingstmt

---
+++ loop.java
@@ -0,0 +1,13 @@
+public class loop
+{
+    public static void main(String[] args)
+    {
+    int i=0;
+    System.out.println("The numbers are");
+    while(i<=10)
+    {
+        System.out.println(i);
+        i++;
+    }
+    }
+}
