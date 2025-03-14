0497330fa966341b121ab34149a61e861125c800
revathithirumal <revathit2003@gmail.com>
2025-03-14 15:42:59 +0530

    fix arrr

---
+++ forloopar.java
@@ -0,0 +1,9 @@
+public class forloopar{
+    public static void main(String[] args){
+        String fruits[]={"apple","orenge","banana","mango"};
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println(fruits[i]);
+        }
+    }
+}
