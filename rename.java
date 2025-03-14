004163a2e80fa0adaa3a50b3c76a5ac8ffc54245
revathithirumal <revathit2003@gmail.com>
2025-03-14 14:43:41 +0530

    fix fruitor

---
+++ rename.java
@@ -0,0 +1,9 @@
+public class rename
+{
+    public static void main(String[] args)
+    {
+        String fruits[]={"apple","mango"};
+        fruits[1]="orenge";
+        System.out.println(fruits[1]);
+    }
+}
