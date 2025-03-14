23d6f618898cc2557d70636f3675fe1ded5a4cd2
revathithirumal <revathit2003@gmail.com>
2025-03-14 10:45:26 +0530

    fix ten print

---
+++ ten.java
@@ -0,0 +1,13 @@
+public class ten
+{
+    public static void main(String[] args)
+    {
+    int i=1;
+    System.out.println("The numbers are");
+    while(i<=5)
+    { 
+        System.out.println(10);
+        i++;
+    }
+    }
+}
