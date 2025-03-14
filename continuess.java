cc6449d061bccfee76575a132add7e01b4087a10
revathithirumal <revathit2003@gmail.com>
2025-03-14 11:52:24 +0530

    fix continue

---
+++ continuess.java
@@ -0,0 +1,14 @@
+public class continuess
+{
+    public static void main(String[] args)
+    {
+        for(int i=0;i<=10;i++)
+        {
+        if(i==7)
+        {
+            continue;
+        }
+        System.out.println(i); 
+    }
+}
+}
