26ec720d2fcee9d41d6cdc784bb21b404f7f92bc
revathithirumal <revathit2003@gmail.com>
2025-03-14 13:55:48 +0530

    fix arlen

---
+++ lenarray.java
@@ -0,0 +1,6 @@
+public class lenarray{
+    public static void main(String[] args){
+        String animals[]={"dog","cat","lion","monkey","tiger"};
+        System.out.println(animals.length);
+    }
+}
