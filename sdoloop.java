c75225100547be2a486b1206a93bfa203d977bc6
revathithirumal <revathit2003@gmail.com>
2025-03-14 10:57:22 +0530

    fix orenge

---
+++ sdoloop.java
@@ -0,0 +1,13 @@
+public class sdoloop
+{
+    public static void main(String[] args)
+    {
+        int i=1;
+        String fruit="orenge";
+        do{
+            System.out.println(fruit);
+            i++;
+        }
+        while(i<=5);
+    }
+}
