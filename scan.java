26533780e05b04e2592fefbe1f50c95287997b03
revathithirumal <revathit2003@gmail.com>
2025-03-15 12:04:25 +0530

    fix scan name

---
+++ scan.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class scan{
+    public static void main(String[] args){
+        Scanner s1=new Scanner(System.in);
+        System.out.println("Enter username");
+        String username=s1.nextLine();
+        System.out.println("The user name is:"+username);
+    }
+}
