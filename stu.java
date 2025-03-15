f804eb1e8e6dba594f44507de9600a69b4a9f6cf
revathithirumal <revathit2003@gmail.com>
2025-03-15 10:13:50 +0530

    fix student

---
+++ stu.java
@@ -0,0 +1,13 @@
+class student{
+    int id;
+    String name;
+}
+public class stu{
+    public static void main(String[] args){
+        student s1=new student();
+        s1.id=101;
+        s1.name="revathi";
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+    }
+}
