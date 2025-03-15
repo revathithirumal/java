111f3afbf7d2c546f6c0e5057e03ce16996ceffa
revathithirumal <revathit2003@gmail.com>
2025-03-15 11:35:22 +0530

    fix coffee

---
+++ coffeeshop.java
@@ -0,0 +1,25 @@
+public class coffeeshop{
+    public static void main(String[] args){
+        int coffee=2;
+        switch(coffee) {  
+            case 1:
+                System.out.println("cool coffee");
+                break;
+            case 2:
+                System.out.println("choclate coffee");
+                break;
+            case 3:
+                System.out.println("lite coffee");
+                break;
+            case 4:
+                System.out.println("darck coffe");
+                break;
+            case 5:
+                System.out.println("normal coffee");
+                break;
+            default:
+                System.out.println("coffee");
+                break;
+        }
+    }
+}
