c470d68f5a6404b43049c8bebd6d4a7eda8e373b
revathithirumal <revathit2003@gmail.com>
2025-03-15 10:30:01 +0530

    fix bill

---
+++ billing.java
@@ -0,0 +1,12 @@
+public class billing{
+    public static void  main(String[] args){
+        int invoiceno[]={100,101,102,103,104};
+        {
+            System.out.println("The invoice number:");
+            for(int i=0;i<=5;i++)
+            {
+                System.out.println(invoiceno[i]);
+            }
+    }
+}
+}
